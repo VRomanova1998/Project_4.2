f30867634f2fd17cd39eafe45c7d60f177a3cefc
Valeriia Romanova <gee98@mail.ru>
2024-11-12 01:09:33 +0300

    fourth

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
 const input = document.querySelector('.input_search');
 const searchResults = document.querySelector('.container');
 const infoContainer = document.querySelector('.infoContainer');
+const errorMessage = document.querySelector('.error_403');
+
+
+const debounce = (fn, debounceTime) => {
+  let timer;
+  return function(){
+    clearTimeout(timer);
+   timer = setTimeout(()=>{
+      fn();
+    }, debounceTime)
+  }
+};
+
+// Функция обработки результата запроса на сервер
+async function getResponse(value) {
+    const maxRequest = 5;
+    let attempt = 0;
+
+    while (attempt < maxRequest) {
+        try {
+          let response = await fetch(`https://api.github.com/search/repositories?q=${value}`, {
+                  headers: {
+                    'Authorization': 'token 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'
+                }
+                });
+          if (response.ok) {
+            errorMessage.textContent = '';
+            return response;
+          } else {
+          errorMessage.textContent = 'Ошибка. Ждем перед повторной попыткой...';
+          console.log('Ошибка. Ждем перед повторной попыткой...');
+          await new Promise(resolve => setTimeout(resolve, 3000));
+          }
+        } catch(err){
+          errorMessage.textContent = 'Ошибка. Ждем перед повторной попыткой...';
+          console.error('Ошибка во время запроса:', error);
+          await new Promise(resolve => setTimeout(resolve, 3000));
+        }
+attempt++;
+    }
+}
 
 // Создание блока с информацией по выбранному репозиторию
 function createInfoBlock (e){
@@ -16,8 +57,7 @@ function createInfoBlock (e){
 }
 
 // Функция отправки и обработки запроса на сервер
-function queryResult(){
-setTimeout(async()=>{
+async function queryResult(){
   try {
     let value = document.querySelector('.input_search').value;
       if (value == '' || value == ' ') {
@@ -25,7 +65,7 @@ setTimeout(async()=>{
         return;
       }
     let format = new RegExp(`^${value}`, 'i');
-    let response = await fetch(`https://api.github.com/search/repositories?q=${value}`);
+    let response = await getResponse(value);
     document.querySelectorAll('.search_result').forEach(element => {element.remove()});
     let json = await response.json();
     let count = 0;
@@ -44,9 +84,9 @@ setTimeout(async()=>{
         } 
       } 
     } catch(error) {
-      console.log(`${error.name}: ${error.message}`);
+      alert('количество запросов слишком большое');
     }
-}, 400)
+
 }
 
  // Обработка события "удалить информационный блок" 
@@ -58,5 +98,4 @@ setTimeout(async()=>{
      tag.parentElement.remove();
   }, true)
 
-  input.addEventListener('keydown', queryResult);
-
+  input.addEventListener('keydown', debounce(queryResult, 900));
